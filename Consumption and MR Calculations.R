558ae7d4596e59d4c18f5b04a542923db07b061a
LauraSFletcher <laurasfletcher@gmail.com>
2024-07-09 12:10:13 -0600

    Add files via upload

---
+++ Consumption and MR Calculations.R
@@ -0,0 +1,78 @@
+### MODEL PARAMETERIZATION ###
+
+##Consumption and Distributions; Latitudinal Project
+
+rm(list = ls()) ##resets R so that nothing transfers over from past iterations
+Latitude <- read.csv("~/Documents/Hemi DSVM Final/Datasets and Calculations/Hemigrapsus Latitudinal Project.csv", header = T) #import data
+attach(Latitude) #attach the file so column names can be called independently of file name
+names(Latitude) #list column names in file
+
+#Consumption; Bailey Island, ME
+NH <- subset(Latitude, CollectionSite=="NH")
+#n=157
+
+summary(nls(Gut~a*exp(b*Body),start=c(a=0.1,b=2),data=NH))
+
+C_g <- 0.019610*exp(0.470617*bodymass)
+
+#Distributions for Monte Carlo simulations (fixed vs. optimal). 
+mean(Body) 
+sd(Body) 
+#Mean = 1.396818 and Standard Deviation = 0.7582952
+
+summary(lm(P~Body,data=Latitude))
+
+#P_mass <- 0.10656*bodymass - 0.013258 
+
+
+##Metabolism; July 2022, Bailey Island, ME and Odiorne Pt, NH
+
+rm(list = ls()) ##resets R so that nothing transfers over from past iterations
+detach(Maine)
+Maine <- read.csv("~/Documents/Hemi DSVM Final/Datasets and Calculations/Maine Data.csv", header = T) #import data
+attach(Maine) #attach the file so column names can be called independently of file name
+names(Maine) #list column names in file
+
+Maine <- subset(Maine, Sex == "F" & Species == "H.s") #remove Carcinus and male individuals from dataset
+#n=423
+
+#split dataset into gravid and nongravid females
+gravid_females <- subset(Maine,Gravid=="1")
+#n=280
+nongravid_females <- subset(Maine,Gravid=="0")
+#n=138
+
+#MR for gravid females
+summary(lm(MR~Body, data=gravid_females))
+
+R_air <- 24*(0.107595 + bodymass*0.150346)
+
+#MR for nongravid females
+summary(lm(MR~Body, data=nongravid_females))
+
+R_air <- 24*(0.0862 + bodymass*0.16135)
+
+
+Maine <- subset(Maine,Body > 0) #n = 420
+
+summary(lm(Body~WetMass,data=Maine))
+
+plot(Body~WetMass,data=Maine)
+abline(lm(Body~WetMass))
+
+Body <- Maine$Body
+WetMass <- as.numeric(Maine$WetMass)
+
+
+summary(lm(Body~WetMass))
+
+#bodymass <- 0.32572*wetmass -0.138044
+
+
+
+
+
+
+
+
+
---
+++ Energy Calculations.R
@@ -0,0 +1,46 @@
+rm(list = ls()) #clear memory
+detach(Latitudinal)
+Latitudinal <- read.csv("~/Documents/Hemi DSVM Final/Datasets and Calculations/Hemigrapsus Latitudinal Project.csv", header = T) #import data
+attach(Latitudinal) #attach the file so column names can be called independently of file name
+names(Latitudinal) #list column names in file
+
+plot(P~Body,data=Latitudinal)
+abline(0,0.17,col="purple") #max line (ignoring outliers) (To determine Pmax)
+abline(0,0.03,col="green") #min line (To determine Pmin)
+
+
+energy <- subset(Latitudinal,Penergy_kJ>0) #n = 243
+
+#p_max <- 0.17*bodymass #Maximum mass of hepatopancreas tissue (taken from samples collected throughout the invaded range in 2020, n = 799; Griffen et al. 2022)
+#p_min <- (0.03*bodymass #Minimum mass of hepato/ovary tissue (taken from samples collected throughout the invaded range in 2020, n = 799; Griffen et al. 2022)
+#P_joules <- 32.1672-(1.0387*bodymass) #kJ/g of hepato/ovary tissue (taken from calorimetry data, n = 245; to be published elsewhere)
+#P_max <- p_max * P_joules #Maximum energy of hepato/ovary tissue 
+#P_min <- p_min * P_joules #Minimum energy of hepato/ovary tissue 
+
+summary(lm(P_kJ_g~Body, data=Latitudinal))
+plot(P_kJ_g~Body, data=Latitudinal)
+abline(lm(P_kJ_g~Body, data=Latitudinal))
+
+#P_joules <- 32.1672-(1.0387*bodymass) #kJ/g
+
+eggs <- subset(Latitudinal, Eenergy>0) #n = 213 clutches
+
+E_energy <- as.numeric(eggs$Eenergy)
+Body <- eggs$Body
+E_kJ <- E_energy/(eggs$Eggs)
+
+summary(lm(E_kJ~Body))
+
+#E_kJ <- 32.9725-(2.388*Body)
+
+plot(Eenergy~Body,data=eggs)
+abline(0.5,3,col="red")
+
+#max_eggs <- (3 * bodymass) + 0.5
+
+#Starting energy for monte carlo simulations
+gravid <- subset(energy,Gravid==1) #n = 83
+
+summary(lm(Penergy_kJ~Body,data=gravid))
+
+#P_energy <- (2.8544*bodymass) + 1.1748
